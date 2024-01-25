82930a167e000dfe4a9adf630ad2fad041ba906e
morronepablo <morronepablo@gmail.com>
2024-01-24 23:44:28 -0300

    creating OurStore

---
+++ ecommerce-app/src/pages/OurStore.js
@@ -0,0 +1,46 @@
+import React from "react";
+import BrandCrumb from "../components/BrandCrumb";
+import Meta from "../components/Meta";
+
+const OurStore = () => {
+  return (
+    <>
+      <Meta title={"Out Store"} />
+      <BrandCrumb title="Our Store" />
+      <div className="store-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Shop By Categories</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li>Watch</li>
+                    <li>Tv</li>
+                    <li>Camera</li>
+                    <li>Laptop</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Filter By</h3>
+                <div>
+                  <h5 className="sub-title">Availability</h5>
+                </div>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Product Tags</h3>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Random Product</h3>
+              </div>
+            </div>
+            <div className="col-9"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OurStore;
