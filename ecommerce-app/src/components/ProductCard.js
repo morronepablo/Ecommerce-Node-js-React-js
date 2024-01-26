6c893bf4f2c3dc544d32e83daff4659c5ad2691b
morronepablo <morronepablo@gmail.com>
2024-01-26 16:28:24 -0300

    finality ProductCard and OutStore

--- ecommerce-app/src/components/ProductCard.js
+++ ecommerce-app/src/components/ProductCard.js
@@ -1,58 +1,128 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-const ProductCard = () => {
+const ProductCard = (props) => {
+  const { grid } = props;
+  let location = useLocation();
+
   return (
-    <div className="col-3">
-      <Link className="product-card position-relative">
-        <div className="wishlist-icon position-absolute">
-          <Link>
-            <img src="images/wish.svg" alt="wish" />
-          </Link>
-        </div>
-        <div className="product-image">
-          <img
-            src="images/headphone2.jpg"
-            className="img-fluid"
-            alt="productImage"
-          />
-          <img
-            src="images/headphone3.jpg"
-            className="img-fluid"
-            alt="productImage"
-          />
-        </div>
-        <div className="product-details">
-          <h6 className="brand">Havel's</h6>
-          <h5 className="product-title">
-            Kids Headphones bulk 10 pack multi colored for students
-          </h5>
-          <ReactStars
-            count={5}
-            size={24}
-            isHalf={true}
-            value={"3.5"}
-            edit={false}
-            activeColor="#ffd700"
-          />
-          <p className="price">$100.00</p>
-        </div>
-        <div className="action-bar position-absolute">
-          <div className="d-flex flex-column gap-15">
+    <>
+      <div
+        className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}
+      >
+        <Link className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
             <Link>
-              <img src="images/prodcompare.svg" alt="prodcompare" />
-            </Link>
-            <Link>
-              <img src="images/view.svg" alt="view" />
-            </Link>
-            <Link>
-              <img src="images/add-cart.svg" alt="addcart" />
+              <img src="images/wish.svg" alt="wish" />
             </Link>
           </div>
-        </div>
-      </Link>
-    </div>
+          <div className="product-image">
+            <img
+              src="images/headphone2.jpg"
+              className="img-fluid"
+              alt="productImage"
+            />
+            <img
+              src="images/headphone3.jpg"
+              className="img-fluid"
+              alt="productImage"
+            />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Havel's</h6>
+            <h5 className="product-title">
+              Kids Headphones bulk 10 pack multi colored for students
+            </h5>
+            <ReactStars
+              count={5}
+              size={24}
+              isHalf={true}
+              value={"3.5"}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
+              veniam eligendi numquam optio eveniet quod similique, suscipit
+              necessitatibus sint nobis itaque, minima excepturi ea, omnis ab
+              porro neque deserunt. Similique...
+            </p>
+            <p className="price">$100.00</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <Link>
+                <img src="images/prodcompare.svg" alt="prodcompare" />
+              </Link>
+              <Link>
+                <img src="images/view.svg" alt="view" />
+              </Link>
+              <Link>
+                <img src="images/add-cart.svg" alt="addcart" />
+              </Link>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div
+        className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}
+      >
+        <Link className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
+            <Link>
+              <img src="images/wish.svg" alt="wish" />
+            </Link>
+          </div>
+          <div className="product-image">
+            <img
+              src="images/headphone2.jpg"
+              className="img-fluid"
+              alt="productImage"
+            />
+            <img
+              src="images/headphone3.jpg"
+              className="img-fluid"
+              alt="productImage"
+            />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Havel's</h6>
+            <h5 className="product-title">
+              Kids Headphones bulk 10 pack multi colored for students
+            </h5>
+            <ReactStars
+              count={5}
+              size={24}
+              isHalf={true}
+              value={"3.5"}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
+              veniam eligendi numquam optio eveniet quod similique, suscipit
+              necessitatibus sint nobis itaque, minima excepturi ea, omnis ab
+              porro neque deserunt. Similique...
+            </p>
+            <p className="price">$100.00</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <Link>
+                <img src="images/prodcompare.svg" alt="prodcompare" />
+              </Link>
+              <Link>
+                <img src="images/view.svg" alt="view" />
+              </Link>
+              <Link>
+                <img src="images/add-cart.svg" alt="addcart" />
+              </Link>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
   );
 };
 
--- ecommerce-app/src/pages/OurStore.js
+++ ecommerce-app/src/pages/OurStore.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import BrandCrumb from "../components/BrandCrumb";
 import Meta from "../components/Meta";
+import ReactStars from "react-rating-stars-component";
+import ProductCard from "./../components/ProductCard";
 
 const OurStore = () => {
+  const [grid, setGrid] = useState(4);
+
   return (
     <>
       <Meta title={"Out Store"} />
@@ -26,16 +30,247 @@ const OurStore = () => {
                 <h3 className="filter-title">Filter By</h3>
                 <div>
                   <h5 className="sub-title">Availability</h5>
+                  <div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id=""
+                      />
+                      <label className="form-check-label" htmlFor="">
+                        In Stock (1)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id=""
+                      />
+                      <label className="form-check-label" htmlFor="">
+                        Out of Stock (0)
+                      </label>
+                    </div>
+                  </div>
+                  <h5 className="sub-title">Price</h5>
+                  <div className="d-flex align-items-center gap-10">
+                    <div className="form-floating">
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="floatingInput"
+                        placeholder="From"
+                      />
+                      <label htmlFor="floatingInput">From</label>
+                    </div>
+                    <div className="form-floating">
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="floatingInput1"
+                        placeholder="To"
+                      />
+                      <label htmlFor="floatingInput1">To</label>
+                    </div>
+                  </div>
+                  <h5 className="sub-title">Colors</h5>
+                  <div>
+                    <ul className="colors ps-0">
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                    </ul>
+                  </div>
+                  <h5 className="sub-title">Size</h5>
+                  <div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="color-1"
+                      />
+                      <label className="form-check-label" htmlFor="color-1">
+                        S (2)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="color-2"
+                      />
+                      <label className="form-check-label" htmlFor="color-2">
+                        M (2)
+                      </label>
+                    </div>
+                  </div>
                 </div>
               </div>
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Product Tags</h3>
+                <div>
+                  <div className="product-tags d-flex flex-wrap align-items-center gap-10">
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Headphone
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Laptop
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Mobile
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Wire
+                    </span>
+                  </div>
+                </div>
               </div>
               <div className="filter-card mb-3">
                 <h3 className="filter-title">Random Product</h3>
+                <div>
+                  <div className="random-products d-flex pb-3">
+                    <div className="w-50">
+                      <img
+                        className="img-fluid"
+                        src="images/headphone.jpg"
+                        alt="watch"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>
+                        Kids headphones bulk 10 pack multi colored for students
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        isHalf={true}
+                        value={"3.5"}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>$ 300.00</b>
+                    </div>
+                  </div>
+                  <div className="random-products d-flex pb-3 mt-3">
+                    <div className="w-50">
+                      <img
+                        className="img-fluid"
+                        src="images/headphone.jpg"
+                        alt="watch"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>
+                        Kids headphones bulk 10 pack multi colored for students
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        isHalf={true}
+                        value={"3.5"}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>$ 300.00</b>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="col-9"></div>
+            <div className="col-9">
+              <div className="filter-sort-grid mb-4">
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="mb-0 d-block" style={{ width: "100px" }}>
+                      Sort By:{" "}
+                    </p>
+                    <select className="form-control form-select" name="" id="">
+                      <option value="manual">Featured</option>
+                      <option value="best-selling" selected="selected">
+                        Best selling
+                      </option>
+                      <option value="title-ascending">
+                        Alphabetically, A-Z
+                      </option>
+                      <option value="title-descending">
+                        Alphabetically, Z-A
+                      </option>
+                      <option value="price-ascending">
+                        Price, low to high
+                      </option>
+                      <option value="price-descending">
+                        Price high to low
+                      </option>
+                      <option value="created-ascending">
+                        Date, old to new
+                      </option>
+                      <option value="created-descending">
+                        Date, new to old
+                      </option>
+                    </select>
+                  </div>
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="totalproducts mb-0">21 Products</p>
+                    <div className="d-flex gap-10 align-items-center grid">
+                      <img
+                        onClick={() => {
+                          setGrid(3);
+                        }}
+                        className="d-block img-fluid"
+                        src="images/gr4.svg"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(4);
+                        }}
+                        className="d-block img-fluid"
+                        src="images/gr3.svg"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(6);
+                        }}
+                        className="d-block img-fluid"
+                        src="images/gr2.svg"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(12);
+                        }}
+                        className="d-block img-fluid"
+                        src="images/gr.svg"
+                        alt="grid"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="products-list pb-5">
+                <div
+                  className={`d-flex gap-10 flex-wrap ${
+                    grid === 6 ? "justify-content-between" : ""
+                  }`}
+                >
+                  <ProductCard grid={grid} />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
