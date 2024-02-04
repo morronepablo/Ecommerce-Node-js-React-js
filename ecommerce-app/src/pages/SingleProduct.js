7244422769ae41db0a09ec855344f53248309c62
morronepablo <morronepablo@gmail.com>
2024-02-04 18:01:25 -0300

    Adjust SingleProduct

--- ecommerce-app/src/pages/SingleProduct.js
+++ ecommerce-app/src/pages/SingleProduct.js
@@ -22,8 +22,8 @@ const SingleProduct = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
+              <h4>Description</h4>
               <div className="bg-white p-3">
-                <h4>Description</h4>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
                   aspernatur at voluptatibus cumque modi ullam eaque nobis
@@ -35,32 +35,87 @@ const SingleProduct = () => {
           </div>
         </div>
       </div>
-      <section className="reviews-wrapper home-wrapper-2 py-5">
+      <section className="reviews-wrapper home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
-              <div className="review-head d-flex justify-content-between align-items-end">
-                <div>
-                  <h4 className="mb-2">Customer Reviews</h4>
-                  <div className="d-flex gap-10 align-items-center">
-                    <ReactStars
-                      count={5}
-                      size={24}
-                      isHalf={true}
-                      value={"3.5"}
-                      edit={false}
-                      activeColor="#ffd700"
-                    />
-                    <p className="mb-0">Based on 2 Reviews</p>
+              <h3>Reviews</h3>
+              <div className="review-inner-wrapper">
+                <div className="review-head d-flex justify-content-between align-items-end">
+                  <div>
+                    <h4 className="mb-2">Customer Reviews</h4>
+                    <div className="d-flex gap-10 align-items-center">
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        isHalf={true}
+                        value={"3.5"}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <p className="mb-0">Based on 2 Reviews</p>
+                    </div>
+                  </div>
+                  {orderedProduct && (
+                    <div>
+                      <a
+                        className="text-dark text-decoration-underline"
+                        href=""
+                      >
+                        Write a Review
+                      </a>
+                    </div>
+                  )}
+                </div>
+                <div className="review-form py-4">
+                  <h4>Write a Review</h4>
+                  <form action="" className="d-flex flex-column gap-15">
+                    <div>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        isHalf={true}
+                        value={"3.5"}
+                        edit={true}
+                        activeColor="#ffd700"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        className="w-100 form-control"
+                        name=""
+                        id=""
+                        cols="30"
+                        rows="4"
+                        placeholder="Comments"
+                      ></textarea>
+                    </div>
+                    <div className="d-flex justify-content-end">
+                      <button className="button border-0">Submit Review</button>
+                    </div>
+                  </form>
+                </div>
+                <div className="reviews mt-4">
+                  <div className="review">
+                    <div className="d-flex gap-10 align-items-center">
+                      <h6 className="mb-0">Navdeep</h6>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        isHalf={true}
+                        value={"3.5"}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                    </div>
+                    <p className="mt-3">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Dolor quas, accusamus tempore possimus voluptas, nam fugit
+                      nulla, fugiat error adipisci quam ad fuga corrupti beatae
+                      quod. Quod expedita accusamus nihil.
+                    </p>
                   </div>
                 </div>
-                {orderedProduct && (
-                  <div>
-                    <a className="text-dark text-decoration-underline" href="">
-                      Write a Review
-                    </a>
-                  </div>
-                )}
               </div>
             </div>
           </div>
